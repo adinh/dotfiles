df99c9cda45456c177b32781035cd30086a0bfc8
Tan Le <tanle.oz@gmail.com>
2017-09-04 09:12:46 +1000

    Exclude all the quirks.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 set -x FZF_DEFAULT_COMMAND "rg --files --no-ignore --hidden --follow --glob '!.git/*'"
 set -x FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
 
+# Chruby
+source /usr/local/share/chruby/chruby.fish
+chruby ruby-2.4
+source /usr/local/share/chruby/auto.fish
